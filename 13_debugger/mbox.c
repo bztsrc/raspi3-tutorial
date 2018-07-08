214885df63f78ed6de9e43a210d1480c67755b96
Florian La Roche <Florian.LaRoche@googlemail.com>
2018-07-08 14:22:01 +0200

    compute the mailbox cmd only once

--- 13_debugger/mbox.c
+++ 13_debugger/mbox.c
@@ -44,18 +44,17 @@ volatile unsigned int  __attribute__((aligned(16))) mbox[36];
  */
 int mbox_call(unsigned char ch)
 {
-    unsigned int r;
+    unsigned int r = (((unsigned int)((unsigned long)&mbox)&~0xF) | (ch&0xF));
     /* wait until we can write to the mailbox */
     do{asm volatile("nop");}while(*MBOX_STATUS & MBOX_FULL);
     /* write the address of our message to the mailbox with channel identifier */
-    *MBOX_WRITE = (((unsigned int)((unsigned long)&mbox)&~0xF) | (ch&0xF));
+    *MBOX_WRITE = r;
     /* now wait for the response */
     while(1) {
         /* is there a response? */
         do{asm volatile("nop");}while(*MBOX_STATUS & MBOX_EMPTY);
-        r=*MBOX_READ;
         /* is it a response to our message? */
-        if((unsigned char)(r&0xF)==ch && (r&~0xF)==(unsigned int)((unsigned long)&mbox))
+        if(r == *MBOX_READ)
             /* is it a valid successful response? */
             return mbox[1]==MBOX_RESPONSE;
     }
